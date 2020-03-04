273c429faa941cd1eb4d2581ce3c7efc0458179d
David Ehigiator <david.ehigiator@cyberspace.net.ng>
2020-03-04 14:51:43 +0100

    example update

--- Example/cyberpaysdk/ViewController.swift
+++ Example/cyberpaysdk/ViewController.swift
@@ -15,8 +15,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-           CyberpaySdk.shared.initialise(with: "5704d9ab0e114746accc5db8e927821b", mode: .Live)
-        .setTransaction(forCustomerEmail: "david3ti@gmail.com", amountInKobo: 5000)
+           CyberpaySdk.shared.initialise(with: "CYBERPAY_INTEGRATION_KEY", mode: .Debug)
+        .setTransaction(forCustomerEmail: "CUSTOMER_EMAIL", amountInKobo: 5000)
                 .dropInCheckout(rootController: self, onSuccess: {result in
                     print(result.reference)
                     
